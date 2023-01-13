fb9ad7b55922f7f0c69c18f0609fa786d77c53a6
michaelhaaf <michael.haaf@gmail.com>
2023-01-13 08:37:17 -0500

    implement highlighted sticky side scrolling navigation

--- js/main.js
+++ js/main.js
@@ -8,3 +8,24 @@ function toggleTheme() {
   document.querySelector(":root").setAttribute("data-theme", newTheme);
   localStorage.setItem("currentTheme", newTheme);
 }
+
+// Adapted from www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/index.html
+window.addEventListener('DOMContentLoaded', () => {
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).classList.add('active');
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).classList.remove('active');
+			}
+		});
+	});
+
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+	
+});
