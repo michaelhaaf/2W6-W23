00af7e7b06b4b9d5005c4fa85db57f74485d157a
michaelhaaf <michael.haaf@gmail.com>
2023-01-21 15:05:21 -0500

    PARTIAL PROGRESS: alternate implementation of sidescroller. May revert.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,50 @@
+const tocElements = Array.from(
+  document.querySelectorAll(".page-nav a")
+);
+
 let currentTheme = window.matchMedia("(prefers-color-scheme: light)").matches
   ? "light"
   : "dark";
 localStorage.setItem("currentTheme", currentTheme);
 
 function toggleTheme() {
-  let newTheme = localStorage.getItem("currentTheme") === "light" ? "dark" : "light";
+  let newTheme =
+    localStorage.getItem("currentTheme") === "light" ? "dark" : "light";
   document.querySelector(":root").setAttribute("data-theme", newTheme);
   localStorage.setItem("currentTheme", newTheme);
 }
 
-// Adapted from www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/index.html
-window.addEventListener('DOMContentLoaded', () => {
+const getTop = (entry) => Math.abs(entry.boundingClientRect.top);
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).classList.remove('active');
-			}
-		});
-	});
+const setActive = (element) => {
+  element.classList.add("active");
+};
 
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
-	});
-	
+const setInactive = (element) => {
+  element.classList.remove("active");
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    console.log(observer);
+    let activeEntry = entries.reduce((e, c) => (getTop(e) < getTop(c) ? e : c));
+
+    if (!activeEntry.isIntersecting) return;
+    let activeSectionId = activeEntry.target.getAttribute("id");
+    let activeSection = document.querySelector(
+      `nav li a[href="#${activeSectionId}"]`
+    );
+    tocElements.filter((elem) => elem !== activeSection).map(setInactive);
+    setActive(activeSection);
+  });
+
+  // Track all elements in the page content
+  //   - that have an `id` applied
+  //   - that have a corresponding entry in the table of contents
+  // Adapted from www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/index.html
+  document.querySelectorAll("main *[id]").forEach((element) => {
+    if (document.querySelector(`nav li a[href="#${element.id}"]`)) {
+      observer.observe(element);
+    }
+  });
 });
