278a56cbe0c6186a4895bd326998aa72bf3b1aa5
michaelhaaf <michael.haaf@gmail.com>
2023-01-31 02:29:51 -0500

    Significant CSS refactor and behavior change.
    Refactor: section organization enforced, reduction in duplication.
    Not perfect, but much more coherent.
    Behavior: dropdown menu mostly.
    HTML changes also required.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Theme toggle
 if (!localStorage.getItem("selectedTheme")) {
   let currentTheme = window.matchMedia("(prefers-color-scheme: light)").matches
     ? "light"
@@ -19,8 +20,8 @@ themeToggleIcon.addEventListener("click", toggleTheme)
 
 // Side-scroller Table of Contents with "active" section.
 // Adapted from https://benfrain.com/building-a-table-of-contents-with-active-indicator-using-javascript-intersection-observers/
-const pageContent = document.querySelector("article.page-main-article");
-const pageToc = document.querySelector(".article-nav");
+const pageContent = document.querySelector("article.article");
+const pageToc = document.querySelector(".nav--page");
 
 const tocLinks = pageToc.querySelectorAll(":scope a");
 const allHeaders = pageContent.querySelectorAll(":scope > h1, :scope > h2");
@@ -43,4 +44,68 @@ function setActive(entries) {
         .classList.add("active");
     }
   });
+}
+
+// Clickable dropdown: 
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar_click
+
+const dropbtns = Array.from(document.querySelectorAll(".dropbtn"));
+const dropdowns = Array.from(document.querySelectorAll(".dropdown"));
+dropbtns.forEach(btn => { 
+  btn.addEventListener("click", toggleDropdown);
+});
+  
+function toggleVisible(elem) {
+  elem.classList.toggle("hidden");
+}
+
+function hide(elem) {
+  if (!elem.classList.contains("hidden")) {
+    elem.classList.add("hidden");
+  }
+}
+
+function show(elem) {
+  if (elem.classList.contains("hidden")) {
+    elem.classList.remove("hidden");
+  }
+}
+
+function toggleActive(elem) {
+  elem.classList.toggle("active");
+}
+
+function turnInactive(elem) {
+  if (elem.classList.contains("active")) {
+    elem.classList.remove("active");
+  }
+}
+
+function turnActive(elem) {
+  if (!elem.classList.contains("active")) {
+    elem.classList.add("active");
+  }
+}
+
+function toggleDropdown(event) {
+  let dropbtn = event.target;
+  let activeDropdown = dropbtn.nextElementSibling;
+
+  dropdowns
+    .filter(dropdown => dropdown !== activeDropdown)
+    .forEach(dropdown => hide(dropdown));
+  dropbtns
+    .filter(btn => btn !== dropbtn)
+    .forEach(btn => turnInactive(btn));
+
+  toggleVisible(activeDropdown);
+  toggleActive(dropbtn);
+}
+
+// Close the dropdowns if the user clicks outside of them
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn') && !e.target.matches('.dropdown')) {
+    dropdowns.forEach(dropdown => hide(dropdown));
+    dropbtns.forEach(dropbtn => turnInactive(dropbtn));
+  }
 }
